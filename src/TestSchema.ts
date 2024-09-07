4805b29eb276efa25d280a62a1a51c7f90b4ddb2
lee mighdoll <lee@underneath.ca>
2024-09-06 23:16:10 -0700

    copy over some existing tests from wgsl-linker

---
+++ src/TestSchema.ts
@@ -0,0 +1,10 @@
+export interface WgslTestSrc {
+  name: string;                 // human readable description of test
+  src: Record<string, string>;  // source wgsl+ texts, keys are file paths
+  notes?: string;               // additional notes to test implementors
+}
+
+export interface ParsingTest {
+  src: string; 
+  fails?: true;
+}
---
+++ src/index.ts
@@ -0,0 +1,2 @@
+export * from "./test-cases/ImportCases.ts";
+export * from "./test-cases/ImportSyntaxCases.ts";
